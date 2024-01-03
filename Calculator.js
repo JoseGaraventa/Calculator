092c65871f95734beb2cea41214cdb89946e9a46
Jose Garaventa <josemanuelgaraventa@hotmail.com>
2024-01-03 14:55:15 -0300

    done

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+class Calculator {
+
+sumar(num1, nume2) {
+    return num1 + nume2;
+}
+
+restar(num1, num2) {
+    return num1 - num2;
+}
+
+dividir(num1, num2) {
+    return num1 / num2;
+}
+
+multiplicar(num1, num2) {
+    return num1 * num2;
+}
+
+}
